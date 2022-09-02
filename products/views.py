f85d32dbeb508f73254186402b9d0cf31210990d
mjjstockman <mjjstockman@gmail.com>
2022-09-02 17:09:43 +0000

    Add products urlspattern to products/urls and root urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all(request):
+    # products = Product.get_object_or_404.all()
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/all.html', context)
