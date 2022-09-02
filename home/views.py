e5f6aa70fa9c22e3e5717c0d16d4cde547777f29
mjjstockman <mjjstockman@gmail.com>
2022-09-02 09:53:00 +0000

    Add home app, templates, view and url

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    """ A view to return the index page
+
+    Args:
+        request (_type_): the request and template
+    """
+
+    return render(request, 'home/index.html')
+
+
+
