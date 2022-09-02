e37fd13b672e73222bf72d2be88a8c9d7fbde154
mjjstockman <mjjstockman@gmail.com>
2022-09-02 16:04:08 +0000

    Add product and category to products admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Product, Category
 
+# not used as currently displaying all fields in admin.  Change/delete later
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = {
+
+#     }
+
+    # add if want to sort order
+    # ordering = ('',)
+
 admin.site.register(Product)
 admin.site.register(Category)
+
+# if using ProductAdmin class above...
+# admin.site.register(Product, ProductAdmin)
