66b9561ce5779ae83f9cd2a12a90811d08e8a682
mjjstockman <mjjstockman@gmail.com>
2022-09-05 12:31:13 +0000

    Set debug to true if environ had development var

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Only set debug to True if DEVELOPMENT var is in the environ
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ecomm-p5.herokuapp.com', 'localhost']
 
