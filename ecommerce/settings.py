d9442c791ae473f0e63829c3aca382f8e745a81c
mjjstockman <mjjstockman@gmail.com>
2022-09-05 17:58:17 +0000

    Add enctype to products/add template

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -192,6 +192,8 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    # Static and media files
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
