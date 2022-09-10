9c5c522dd80f2674ddb62133ab0ee83196409372
mjjstockman <mjjstockman@gmail.com>
2022-09-10 12:02:35 +0000

    Implement nav searchbar functionality

--- menu/urls.py
+++ menu/urls.py
@@ -4,7 +4,5 @@ from .import views
 
 urlpatterns = [
     path('', views.menu_all, name='menu_all'),
-    # path('<str:pk>/', views.detail, name='detail'),
     path('<str:pk>/', views.detail, name='detail'),
-    # path('detail/', views.detail, name='detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
 from products.models import Product
 
 
 def menu_all(request):
     # products = Product.get_object_or_404.all()
     products = Product.objects.all()
+    # set below as None so query is defined if not set in request and therefore empty in context
+    query = None
 
+    # SEARCH BOX: form text input was named q, so check if q is in request.GET
+    # check if request.get exists (if get param was sent in request from search form in nav, or when selecting category in nav)
+    if request.GET:
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                # messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            # set queries var to Q obj where name or description contains the query, case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            # use filter method with queries var passed to it
+            products = products.filter(queries)
+        
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'menu/menu-all.html', context)
