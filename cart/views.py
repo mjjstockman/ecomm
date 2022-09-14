c1ae699de21aa4a6c4a71ecef445082a36cc2637
mjjstockman <mjjstockman@gmail.com>
2022-09-14 16:16:55 +0000

    Change cart templates, urls and views to use item_id instead of product_id

--- cart/views.py
+++ cart/views.py
@@ -7,7 +7,7 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
-def add_to_cart(request, product_id):
+def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
     # quantity will be a str from template so convert to int
@@ -18,12 +18,12 @@ def add_to_cart(request, product_id):
     cart = request.session.get('cart', {})
 
     # if cart dict already contains the item_id...
-    if product_id in list(cart.keys()):
+    if item_id in list(cart.keys()):
         # ... increment quantity
-        cart[product_id] += quantity
+        cart[item_id] += quantity
     else:
         # or add the item_id with quantity
-        cart[product_id] = quantity
+        cart[item_id] = quantity
 
     # add cart to session
     request.session['cart'] = cart
@@ -36,16 +36,13 @@ def adjust_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
 
-    # create a session
-    # if cart var exists add to session, if not make new cart dict
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
     else:
-        cart.pop[item_id]
+        cart.pop(item_id)
 
-    # add cart to session
     request.session['cart'] = cart
     # print(request.session['cart'])
     return redirect(reverse('view_cart'))
