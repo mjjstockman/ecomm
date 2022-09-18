e406f7b97a802ff576763d1fe079c2191d8078fe
mjjstockman <mjjstockman@gmail.com>
2022-09-18 10:54:10 +0000

    Add checkout/checkout views, urls, templates and css dir

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('menu/', include('menu.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
