4dced32f0dad6eaf3dbb7b3afd638bf2e4415611
mjjstockman <mjjstockman@gmail.com>
2022-09-20 12:47:05 +0000

    Add checkout functionality

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,14 +11,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
-                       'order_total',)
+                       'total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'postcode', 'town_or_city', 
-              'street_address1', 'street_address2', 'order_total',)
+              'street_address1', 'street_address2', 'total',)
 
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total')
+                    'total')
 
     ordering = ('-date',)
 
