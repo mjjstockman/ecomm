1114d64942fa5f497f3795c9b4286171590374d5
mjjstockman <mjjstockman@gmail.com>
2022-09-25 11:25:53 +0000

    Finish add product functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 # from django.contrib.auth.decorators import user_passes_test
 from .models import Product
 from .form import ProductForm
@@ -31,7 +32,17 @@ def detail(request, product_id):
 @staff_member_required(login_url='/')
 def add(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add.html'
     context = {
         'form': form,
