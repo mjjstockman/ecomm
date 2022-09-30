326befba725051b3f73e56f905a442f53458e23a
mjjstockman <mjjstockman@gmail.com>
2022-09-30 11:13:23 +0000

    Add category nav to base template and functionality

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 # from django.contrib.auth.decorators import user_passes_test
-from .models import Product
+from .models import Product, Category
 from .form import ProductForm
 
 
@@ -13,20 +13,27 @@ def all(request):
     products = Product.objects.all()
     cart = request.session.get('cart', {})
     query = None
+    categories = None
 
     if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+         if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search criteria!")
+                    return redirect(reverse('products'))
+                
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
         'cart': cart
     }
 
