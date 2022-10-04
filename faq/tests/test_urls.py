3ec3930b9bcc9f9bcd62b50ae59bd46670882174
mjjstockman <mjjstockman@gmail.com>
2022-10-04 11:59:54 +0000

    Move faq url tests to faq/tests/test_urls.py

---
+++ faq/tests/test_urls.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from faq.views import view_faq
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_faq_url_is_resolved(self):
+        url = reverse('view_faq')
+        self.assertEqual(resolve(url).func, view_faq)
