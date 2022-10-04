fe7b1d6a938517f51eabf20a5fcae3413359b990
mjjstockman <mjjstockman@gmail.com>
2022-10-04 12:01:43 +0000

    Change tests/test_urls func name to test_faq_url_resolves

--- faq/tests/test_urls.py
+++ faq/tests/test_urls.py
@@ -5,6 +5,6 @@ from faq.views import view_faq
 
 class TestUrls(SimpleTestCase):
 
-    def test_faq_url_is_resolved(self):
+    def test_faq_url_resolves(self):
         url = reverse('view_faq')
         self.assertEqual(resolve(url).func, view_faq)
