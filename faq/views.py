03f1d9c21fb1286dd240016b31c90f7b840b3154
mjjstockman <mjjstockman@gmail.com>
2022-10-05 16:40:29 +0000

    Remove func based view from faq/views

--- faq/views.py
+++ faq/views.py
@@ -6,9 +6,8 @@ from .models import Question, Answer
 class QuestionList(generic.ListView):
     model = Question
     queryset = Question.objects.filter(status=1).order_by("-created_on")
-    template_name = "view_faq.html"
+    template_name = "faq/view_faq.html"
     paginate_by = 10
 
-def view_faq(request):
-    return render(request, 'faq/view_faq.html')
+
 
