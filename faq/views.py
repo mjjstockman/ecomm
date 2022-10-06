220991a5bbd0773518942f1d5673486acb451c99
mjjstockman <mjjstockman@gmail.com>
2022-10-06 12:30:08 +0000

    Show published questions in faq template

--- faq/views.py
+++ faq/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .forms import QuestionForm
+from .models import Question
 
 def view_faq(request):
 
+    questions = Question.objects.all().order_by('created_on')
     form = QuestionForm()
-    
+
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -12,7 +14,8 @@ def view_faq(request):
 
 
     context = {
-        'form': form
+        'form': form,
+        'questions': questions
     }
     return render(request, 'faq/view_faq.html', context)
 
