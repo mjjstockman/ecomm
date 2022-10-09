14b78fffb90a8746a039a13c5262efa0352cef1e
mjjstockman <mjjstockman@gmail.com>
2022-10-09 13:18:18 +0000

    Fix error caused by deleting migrations

--- faq/models.py
+++ faq/models.py
@@ -24,7 +24,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True, related_name='question_answer')
     body = models.CharField(max_length=200, unique=True)
     # author = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, related_name="answer_author"
+    #     User, related_name="answer_author", on_delete=models.SET_NULL, null=True
     # )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
