5ac58f3b5105bc0b2e6d34010a4d7d0f0a71fac7
mjjstockman <mjjstockman@gmail.com>
2022-10-14 19:42:59 +0000

    Correct faq/models line length

--- faq/models.py
+++ faq/models.py
@@ -41,9 +41,9 @@ class Answer(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     like = models.ManyToManyField(User, related_name='like_answer',
-                                   blank=True)
+                                  blank=True)
     dislike = models.ManyToManyField(User, related_name='dislike_answer',
-                                      blank=True)
+                                     blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -55,7 +55,8 @@ class Answer(models.Model):
         if self.status == 1 and self.question.email_on_answer:
             question = self.question.body
             subject = 'Your question on Get Wurst has been answered!'
-            message = f'Your question { question } on Get Wurst has been answered!'
+            message = f'Your question { question } on Get Wurst has been ' \
+                'answered!'
             email_from = settings.EMAIL_HOST_USER
             user_email = [self.question.author.email, ]
             send_mail(subject, message, email_from, user_email)
