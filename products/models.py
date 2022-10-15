30651f71ab92244beb7f8bf0f2aef77d5975e773
mjjstockman <mjjstockman@gmail.com>
2022-10-15 20:16:24 +0000

    Change products description field to short_description

--- products/models.py
+++ products/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', default=0,
                  on_delete=models.SET_DEFAULT)
     name = models.CharField(max_length=254)
+    short_description = models.TextField(default="a")
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='media/product_images')
