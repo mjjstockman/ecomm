51fd7975bc9c6b1090ae0cb5141e10a885c364dc
mjjstockman <mjjstockman@gmail.com>
2022-10-19 14:04:12 +0000

    Add setUp, test_products_all and test_products_detail to products test_views

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -6,11 +6,42 @@ from products.models import Category, Product
 
 class TestViews(TestCase):
 
-    def test_products_all_GET(self):
-        client = Client()
+    def setUp(self):
+        self.client = Client()
+        self.category1 = Category.objects.create(
+            name = 'Category 1',
+        )
+        self.products_url = reverse('products')
+        self.products_detail_url = reverse('product_detail', args=[1])
+        self.product1 = Product.objects.create(
+            name = 'Product 1',
+            short_description = 'Prod 1 description',
+            description = 'Prod 1 full description',
+            price = 1,
+            image = 'media/product_images/test.png',
+            category = self.category1
+        )
 
-        response = client.get(reverse('products'))
+    def test_products_all_GET(self):
+        
+        response = self.client.get(self.products_url)
 
         self.assertEqual(response.status_code, 302)
         self.assertTemplateUsed('products/all.html')
+
+
+    def test_products_detail_GET(self):
+       
+        response = self.client.get(self.products_url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('products/detail.html')
+
+
+    # def test_products_add_POST(self):
+       
+    #     response = self.client.get(self.products_url)
+
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertTemplateUsed('products/add.html')
 
