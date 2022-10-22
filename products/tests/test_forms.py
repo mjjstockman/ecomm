a3272d2f0c31fd4463886c8c1302992aca498f2d
mjjstockman <mjjstockman@gmail.com>
2022-10-22 09:20:33 +0000

    Add str, fk and img to products test_fake_form_valis_data test

---
+++ products/tests/test_forms.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from products.form import FakeForm
+from products.models import Category
+
+
+class TestFakeForm(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.category1 = Category.objects.create(
+            name='Category 1',
+        )
+
+    def test_fake_form_valid_data(self):
+        form = FakeForm(data={
+            'name': 'Fake Name 1',
+            'category': self.category1,
+            'image': SimpleUploadedFile('file.jpg', b"file_content", content_type='image/jpeg')
+        })
+
+        self.assertTrue(form.is_valid())
