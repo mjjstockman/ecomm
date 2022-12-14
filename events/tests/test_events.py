cc4ff3de0127d07626c9266e0cfe8db4693ea53a
mjjstockman <mjjstockman@gmail.com>
2022-12-14 15:40:23 +0000

    Change test_canAddEventDate to test_canAddEventLink

--- events/tests/test_events.py
+++ events/tests/test_events.py
@@ -4,3 +4,8 @@ from events.models import Event
 def test_canAddEventName():
     event = Event()
     event.addName("Glasto")
+
+
+def test_canAddEventLink():
+    event = Event()
+    event.addLink("https://tramlines.org.uk/")
