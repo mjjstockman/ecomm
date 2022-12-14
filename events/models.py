4672aefdfbb6ac1d5fe9535d8b203fcf61308c9a
mjjstockman <mjjstockman@gmail.com>
2022-12-14 15:01:58 +0000

    Add failing test_canAddEventName

--- events/models.py
+++ events/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 
-class Event:
+class Event(models.Model):
     pass
--- events/tests/test_events.py
+++ events/tests/test_events.py
@@ -3,3 +3,8 @@ from events.models import Event
 
 def test_canInstantiateEvent():
     event = Event()
+
+
+def test_canAddEventName():
+    event = Event()
+    event.addName("Glasto")
