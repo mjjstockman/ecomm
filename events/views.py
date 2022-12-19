08dcdd7b7ed89b706f2bf40885f01e79e97366b3
mjjstockman <mjjstockman@gmail.com>
2022-12-19 11:03:39 +0000

    Add events all view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+
+def all(request):
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'events/all.html', context)
