4d9e9a22aa7a056bab71032969c9776f6f6f399c
mjjstockman <mjjstockman@gmail.com>
2022-12-19 12:42:19 +0000

    Add event image and link to model

--- events/models.py
+++ events/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=150, null=True)
+    image = models.ImageField(upload_to='events', null=True)
+    link = models.URLField(max_length=200, null=True)
+    
  
