7ca575acd88c9338ded76274e2920f79537b9316
mjjstockman <mjjstockman@gmail.com>
2022-12-19 13:20:19 +0000

    Show event info in events all template

--- events/models.py
+++ events/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=150, null=True)
-    image = models.ImageField(upload_to='media/product_images')
+    image = models.ImageField(upload_to='events', null=True)
+    link = models.URLField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
  
