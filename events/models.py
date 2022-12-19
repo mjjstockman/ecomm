e926637defa1cbd814bbd5566e9f953979c1f99d
mjjstockman <mjjstockman@gmail.com>
2022-12-19 15:23:34 +0000

    Remove null=false on event model fields

--- events/models.py
+++ events/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=150, null=True)
-    image = models.ImageField(upload_to='events', null=True)
-    link = models.URLField(max_length=200, null=True)
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='events')
+    link = models.URLField(max_length=200)
 
     def __str__(self):
         return self.name
