fa92b22a4146a762414e64f06c84b148ae57a5c5
mjjstockman <mjjstockman@gmail.com>
2022-12-24 14:47:32 +0000

    Remove all type='text/javascript' declerations

--- faq/views.py
+++ faq/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import (render, redirect, get_object_or_404,
+                              get_list_or_404)
 from .forms import QuestionForm, AnswerForm
 from .models import Question, Answer
 
