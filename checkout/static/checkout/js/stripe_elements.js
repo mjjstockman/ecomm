6c958d3e4ebd5866c5138686c394740878cdbbbf
mjjstockman <mjjstockman@gmail.com>
2022-12-25 11:27:05 +0000

    Delete requirements copy.txt, use es6 in jshint in stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,6 +5,8 @@
     https://stripe.com/docs/stripe-js
 */
 
+/*jshint esversion: 6 */
+
 // slice off the quotation marks
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
@@ -111,5 +113,5 @@ form.addEventListener('submit', function (ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
