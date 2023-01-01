c3e0fd220d85c3705b450332c73fca12e1bf7f23
mjjstockman <mjjstockman@gmail.com>
2023-01-01 14:39:41 +0000

    Add 404 page handler

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,8 @@
-from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +18,4 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'ecommerce.views.handler404'
