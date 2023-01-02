0c7c35cea87f9fd16fb6b547bb828478fad4fc77
mjjstockman <mjjstockman@gmail.com>
2023-01-02 11:02:07 +0000

    Add date field to event model

--- events/models.py
+++ events/models.py
@@ -5,6 +5,8 @@ class Event(models.Model):
     name = models.CharField(max_length=150)
     image = models.ImageField(upload_to='events')
     link = models.URLField(max_length=200)
+    date = models.DateField(auto_now_add=False, auto_now=False, blank=True,
+                            null=True)
 
     def __str__(self):
         return self.name
