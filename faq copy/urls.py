b7ae62829892f89ff1aa2eeb826cebcf51ff6ae6
mjjstockman <mjjstockman@gmail.com>
2023-02-07 19:12:06 +0000

    Delete unwanted faq copy app

--- faq copy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('', views.faq, name='faq'),
-    path('answer/<str:pk>', views.answer, name='answer'),
-    path('question/', views.question, name='question'),
-    path('like/<str:pk>', views.like, name='like'),
-]
