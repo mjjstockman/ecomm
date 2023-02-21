d46950ea16832a71fdf2a0ef31285881393ea9e2
mjjstockman <mjjstockman@gmail.com>
2023-02-21 10:50:30 +0000

    Clean cart/contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,3 @@
-# from decimal import Decimal
-# from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
@@ -22,24 +20,10 @@ def cart_contents(request):
             'product': product,
         })
 
-    # if total < settings.FREE_DELIVERY_THRESHOLD:
-    #     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE
-    # / 100)
-    #     free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    # else:
-    #     delivery = 0
-    #     free_delivery_delta = 0
-
-    # grand_total = delivery + total
-
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
-        # 'delivery': delivery,
-        # 'free_delivery_delta': free_delivery_delta,
-        # 'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        # 'grand_total': grand_total,
     }
 
     return context
